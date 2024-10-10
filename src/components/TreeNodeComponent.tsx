66cc1e81504c4cfaa08f5565bfc5fd8e37dfda93
Tegan Jain <teganjain527@gmail.com>
2024-10-10 16:33:06 +0530

    fix: check that items/children exist before printing anything random zeroes would be printed otherwise

--- src/components/TreeNodeComponent.tsx
+++ src/components/TreeNodeComponent.tsx
@@ -8,14 +8,14 @@ export const TreeNode = (props: Props) => {
   return (
     <li>
       {props.node.name}
-      {props.node.children!.length && (
+      {props.node.children && props.node.children.length > 0 && (
         <ul>
           {props.node.children!.map(child => (
             <TreeNode key={child.id} node={child} />
           ))}
         </ul>
       )}
-      {props.node.items!.length && (
+      {props.node.items && props.node.items.length > 0 && (
         <ul>
           {props.node.items!.map((item) => (
             <li key={item.item_id}>
